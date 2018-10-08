b77f8075c0c5a192ce098029cebee5f66e4fc8e9
PierceBurrell <43850056+PierceBurrell@users.noreply.github.com>
2018-10-08 20:12:29 +0100

    Setting Up Basic Code For Ultrasound Sensor
    Initialising Pins and Pin Modes + Basic Loop

--- Ultrasound_Sensor_Code.ino
+++ Ultrasound_Sensor_Code.ino
@@ -1,12 +1,33 @@
+int triggerPin = 11; // Initialising Pin 11
+int echoPin = 12 ; // Initialising Pin 12
+
+long duration;
+long cm;
+long inches;
+
+
+
 void setup() 
 {
   // put your setup code here, to run once:
-  Yooy
+  Serial.begin(9600); // Start Serial Monitor
 
+  pinMode(triggerPin, OUTPUT); // Set PinMode To Output
+  pinMode(echoPin, INPUT); // Set PinMode To Input
+
+  
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
+  digitalWrite(triggerPin, LOW); // Turn Off Trigger Pin
+  delayMicroseconds(5); // Wait 5 ms
+
+  digitalWrite(triggerPin, HIGH); // Turn On Trigger Pin
+  delayMicroseconds(10); // Wait 10 ms
+
+  digitalWrite(triggerPin, LOW); // Turn Off Trigger Pin
+  
 
 }
